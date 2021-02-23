c618c99cfd0e328de8267ed3edb6cc55b9561b8a
nt4f04uNd <nt4f04und@gmail.com>
2021-02-23 20:23:56 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 void main() {
   runApp(App());
+  checkCache();
 }
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,4 +15,30 @@ class App extends StatelessWidget {
       ),
     );
   }
+}
+
+var secondsBetweenPulls = 120;
+var stalePeriodInSeconds = 120;
+
+Future<void> checkCache() async {
+  var i = 0;
+  while (i < 50) {
+    CustomCacheManager.instance
+        .getFileStream('https://via.placeholder.com/3', withProgress: false)
+        .where((e) => e is FileInfo) 
+        .forEach((element) {
+      var fileInfo = element as FileInfo;
+      print("${fileInfo.originalUrl} from: ${fileInfo.source}");
+    });
+
+    await Future.delayed(Duration(seconds: secondsBetweenPulls));
+    i++;
+  }
+}
+
+class CustomCacheManager {
+  static const key = 'customCacheObject';
+  static CacheManager instance = CacheManager(Config(key, 
+    stalePeriod: Duration(seconds: stalePeriodInSeconds), maxNrOfCacheObjects: 1000
+  ));
 }
