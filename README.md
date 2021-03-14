fdd9922507474b3e44f33a3ee2de573af3cd35d7
nt4f04uNd <nt4f04und@gmail.com>
2021-03-14 18:16:32 +0300

    added repro

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-issue <!-- todo: link -->
+issue https://github.com/flutter/flutter/issues/72508
 
-<!-- todo: 
-* change 'flutter_issue' to appropriate branch name
-* and add some breif description gere
--->
+the bug is:
+* **assets/video60_broken** gives black bars from the right and the bottom of the screen
+* all the other videos were re-exported from the adobe premiere and work fine (even 60 fps version)
 
 to clone use
 
 ```
-git clone -b flutter_issue --single-branch git@github.com:nt4f04unds-archive/issues.git
+git clone -b flutter_72508 --single-branch git@github.com:nt4f04unds-archive/issues.git
 ```
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  VideoPlayerController controller;
+  bool ready = false;
+  double height = 100.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _init() async {
+    controller = VideoPlayerController.asset('assets/video60_broken.mp4');
+    await controller.initialize();
+    controller.setLooping(true);
+    controller.play();
+    if (mounted)
+      setState(() {
+        ready = true;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'App',
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.red,
+        body: Stack(
+          children: [
+            ready
+                ? SizedBox(
+                    height: height,
+                    child: VideoPlayer(controller),
+                  )
+                : SizedBox.shrink(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: RaisedButton(
+                child: Text('switch height'),
+                onPressed: () {
+                  if (height == 100.0) {
+                    height = 700.0;
+                  } else {
+                    height = 100.0;
+                  }
+                  setState(() {});
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
